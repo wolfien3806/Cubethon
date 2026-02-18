d0494bddb19cc836bfca629f2ac25b7f4a8368d8
Khanh Wolfie Nguyen <s121228@ltisdschools.net>
2026-02-18 11:59:13 -0600

    Added updates

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    
+    // Update is called once per frame
+
+    //Marked as FixedUpdate since we're just messing around with physisc
+    void FixedUpdate()
+    {
+        //Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow ))
+        {
+            rb.AddForce(150 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-150 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
