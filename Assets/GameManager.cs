ba787d0e5d00f6a1075d6043acd6c122543c2f78
Khanh Wolfie Nguyen <s121228@ltisdschools.net>
2026-02-20 11:58:17 -0600

    Added game manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void gameOver()
+    {
+        Debug.Log("Wow. You suck.");
+    }
+}
